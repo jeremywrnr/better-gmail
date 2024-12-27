45c04e73ef05c48268961567ae9b570a99aefd40
Jeremy Warner <jeremywrnr@gmail.com>
2024-12-26 23:05:25 -0800

    terser

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
-const path = require("path");
-const TerserPlugin = require("terser-webpack-plugin");
+// filepath: /Users/jeremy/Code/betterGmail/webpack.config.js
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
-  mode: "production",
-  entry: "./src/content.js",
+  mode: 'production',
+  entry: './src/content.js',
   output: {
-    filename: "content.bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'content.bundle.js',
+    path: path.resolve(__dirname, 'dist')
   },
   module: {
     rules: [
@@ -14,21 +15,24 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-      },
-    ],
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   },
   optimization: {
     minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        extractComments: false,
-      }),
-    ],
+    minimizer: [new TerserPlugin({
+      extractComments: false,
+      terserOptions: {
+        format: {
+          comments: false,
+        },
+      },
+    })],
   },
-  watch: true,
+  watch: true
 };
