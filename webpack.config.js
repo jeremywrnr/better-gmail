735b84c92f68b0048694590adb1749bd9a6d4284
Jeremy Warner <jeremywrnr@gmail.com>
2024-12-26 18:44:30 -0800

    update gitignore

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/content.js',
+  output: {
+    filename: 'content.bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({
+      extractComments: false,
+    })],
+  },
+  watch: true
+};
